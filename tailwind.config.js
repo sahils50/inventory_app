fdc116872a57d08c15268a6477ee52b2f1ea4ce0
sahil <Sahil.s@truneticinfotech.com>
2026-01-19 12:03:11 +0530

    remove unused layout and implement redirect toggle for login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./hooks/**/*.{js,jsx,ts,tsx}", // If you use classes here
-    "./constants/**/*.{js,jsx,ts,tsx}", // Rare, but safe
+    "./hooks/**/*.{js,jsx,ts,tsx}",
+    "./constants/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
