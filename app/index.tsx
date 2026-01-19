1ff58cc3a85f576174fa6c5c46eefbb918167fad
sahil <Sahil.s@truneticinfotech.com>
2026-01-19 14:33:03 +0530

    fix routing for login and tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
+  //Routes to (app) index and that will decide the next route based on auth status
   return <Redirect href="/(app)" />;
 }
