b2f3c8f63aa46062c3d19288bdb7da9f97e02708
sahil <Sahil.s@truneticinfotech.com>
2026-01-20 16:16:01 +0530

    Implement new pages layout for auth

--- app/(app)/(auth)/_layout.tsx
+++ app/(app)/(auth)/_layout.tsx
@@ -10,6 +10,41 @@ export default function Auth() {
           headerShown: false,
         }}
       />
+      <Stack.Screen
+        name="ForgotPassword"
+        options={{
+          title: "Forgot Password",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="CreateNewPassword"
+        options={{
+          title: "Create New Password",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signup"
+        options={{
+          title: "Sign Up",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="VerifyCode"
+        options={{
+          title: "Verify Code",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Success"
+        options={{
+          title: "Success",
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
