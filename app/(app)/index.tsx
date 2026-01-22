1c58ec6db2ed5266d1ce181c6572ed58c91db52c
sahil <Sahil.s@truneticinfotech.com>
2026-01-22 11:58:08 +0530

    throw user to login as unauthenticated by default

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
-const isLoggedIn = true;
+const isLoggedIn = false;
 export default function Index() {
   if (isLoggedIn) {
     return <Redirect href="/(app)/(tabs)/home" />;
